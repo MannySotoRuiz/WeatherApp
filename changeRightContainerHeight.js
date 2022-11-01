c1037fbb76cfb3337176d2e44e9c437d577aaa5d
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2022-11-01 14:41:30 -0400

    added temp js script to change weather display for weekly/monthly

--- changeRightContainerHeight.js
+++ changeRightContainerHeight.js
@@ -1,1 +1,21 @@
 console.log("Hello World");
+
+
+let howDisplayWeather = document.getElementById("howDisplayWeather");
+let weeklyDisplay = document.getElementById("sevenDayDisplay");
+let monthlyDisplay = document.getElementById("monthlyDisplay");
+
+howDisplayWeather.addEventListener("click", function(event) {
+   let display = event.path[0].innerHTML;
+   if (display == "7 Day") {
+    console.log(weeklyDisplay);
+    monthlyDisplay.classList.add("hidden");
+    weeklyDisplay.classList.remove("hidden");
+   } else if (display == "Monthly") {
+    console.log(monthlyDisplay);
+    weeklyDisplay.classList.add("hidden");
+    monthlyDisplay.classList.remove("hidden");
+   } else {
+    console.log("Hourly");
+   }
+});
