27c5987619dc93be70a9be25c2ecbfcf20dd8072
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2022-11-02 15:32:47 -0400

    buttons working for hourly, monthly, weekly

--- changeRightContainerHeight.js
+++ changeRightContainerHeight.js
@@ -4,6 +4,7 @@ console.log("Hello World");
 let howDisplayWeather = document.getElementById("howDisplayWeather");
 let weeklyDisplay = document.getElementById("sevenDayDisplay");
 let monthlyDisplay = document.getElementById("monthlyDisplay");
+let hourlyDisplay = document.getElementById("hourlyDisplay");
 
 howDisplayWeather.addEventListener("click", function(event) {
    let display = event.path[0].innerHTML;
@@ -11,11 +12,23 @@ howDisplayWeather.addEventListener("click", function(event) {
     console.log(weeklyDisplay);
     monthlyDisplay.classList.add("hidden");
     weeklyDisplay.classList.remove("hidden");
+    hourlyDisplay.classList.add("hidden");
+    document.getElementById("leftContainer").style.height = "900px";
+    document.getElementById("rightContainer").style.height = "900px";
+
    } else if (display == "Monthly") {
     console.log(monthlyDisplay);
     weeklyDisplay.classList.add("hidden");
     monthlyDisplay.classList.remove("hidden");
+    hourlyDisplay.classList.add("hidden");
+    document.getElementById("leftContainer").style.height = "1000px";
+    document.getElementById("rightContainer").style.height = "1000px";
+
    } else {
-    console.log("Hourly");
+    weeklyDisplay.classList.add("hidden");
+    monthlyDisplay.classList.add("hidden");
+    hourlyDisplay.classList.remove("hidden");
+    document.getElementById("leftContainer").style.height = "1830px";
+    document.getElementById("rightContainer").style.height = "1830px";
    }
 });
