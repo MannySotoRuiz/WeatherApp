7b08e9546145d60a6a613f44d626255a36c5b7bb
MannySotoRuiz <72245427+MannySotoRuiz@users.noreply.github.com>
2022-11-10 22:18:26 -0500

    made changes to images on home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 // pages & component
-import Home from "./pages/Home"
+import Home from "./pages/Home";
+import AccountSettings from "./pages/AccountSettings";
+import ErrorPage from "./pages/ErrorPage";
 import Demo from './pages/demo/demo'
 function App() {
   return (
-    <div className="App">
-        <Demo/>
-    </div>
+    // <Router>
+    //   <div className="App">
+    //     <Home/>
+    //   </div>
+    // </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} ></Route>
+        <Route path="/accountsettings" element={<AccountSettings />} ></Route>
+        <Route path="*" element={<ErrorPage />} ></Route>
+      </Routes>
+    </Router>
   );
 }
 
