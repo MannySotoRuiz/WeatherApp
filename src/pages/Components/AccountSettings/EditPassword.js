8450d6f267b056235a0d6805ee3d6b64a2a891c9
MannySotoRuiz <72245427+MannySotoRuiz@users.noreply.github.com>
2022-11-11 22:17:46 -0500

    added show password bttn for user

--- src/pages/Components/AccountSettings/EditPassword.js
+++ src/pages/Components/AccountSettings/EditPassword.js
@@ -1,13 +1,26 @@
+import { useState } from "react";
+
 const EditPassword = () => {
+
+    const [passwordShown, setPasswordShown] = useState(false);
+
+    // Password toggle handler
+    const togglePassword = () => {
+        // when the handler is invoked
+        // inverse the boolean state of passwordShown
+        setPasswordShown(!passwordShown);
+    }
+
     return (
         <div className="hidden" id="passwordDisplay">
             <h2>Password</h2>
             <div id="currentPassword">Current Password<br></br><br></br>
-                <input type="password" id="fcurrentpassword" name="fcurrentpassword"></input><br></br><br></br>
+                <input type={passwordShown ? "text" : "password"} id="fcurrentpassword" name="fcurrentpassword"></input><br></br><br></br>
             </div>
             <div id="newPassword">New Password<br></br><br></br>
-                <input type="password" id="fnewpassword" name="fnewpassword"></input><br></br><br></br>
+                <input type={passwordShown ? "text" : "password"} id="fnewpassword" name="fnewpassword"></input><br></br><br></br>
             </div>
+            <button onClick={togglePassword}>Show Passwords</button>
             <button type="submit" value="Submit">Save</button>
         </div>
     );
