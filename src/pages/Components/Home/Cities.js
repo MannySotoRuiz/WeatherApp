931926ae9e2448c03642c9fbef2c2008525d5014
MannySotoRuiz <72245427+MannySotoRuiz@users.noreply.github.com>
2022-11-13 14:48:40 -0500

    when user clicks on city pic, location change on home right info

--- src/pages/Components/Home/Cities.js
+++ src/pages/Components/Home/Cities.js
@@ -1,24 +1,34 @@
 const Cities = () => {
 
+    const clickedCity = e => {
+        let getLocation = e.currentTarget.parentNode.innerText;
+        let splitLocation = getLocation.split(",");
+        let getCity = splitLocation[0];
+        let getCountry = splitLocation[1].trim(); // to remover the leading white space
+        let newLocation = `${getCity}, ${getCountry}`;
+        let changeLocation = document.getElementById("currentLocation");
+        changeLocation.innerText = newLocation;
+    };
+
     return (
         <div id="cities">
             <div className="selectCity">
-                <div className="cityPic">
+                <div onClick={clickedCity} className="cityPic">
+                    <img src={require('../../../images/CityImages/NYC.jpg')} alt="NYC Pic" />
+                </div>
+                <p className="cityText">New York, USA</p>
+            </div>
+            <div className="selectCity">
+                <div onClick={clickedCity} className="cityPic">
                     <img src={require('../../../images/CityImages/Berlin.jpg')} alt="Berlin Pic" />
                 </div>
                 <p>Berlin, Germany</p>
             </div>
             <div className="selectCity">
-                <div className="cityPic">
+                <div onClick={clickedCity} className="cityPic">
                     <img src={require('../../../images/CityImages/Paris.jpg')} alt="Paris Pic" />
                 </div>
                 <p>Paris, France</p>
-            </div>
-            <div className="selectCity">
-                <div className="cityPic">
-                    <img src={require('../../../images/CityImages/NYC.jpg')} alt="NYC Pic" />
-                </div>
-                <p>New York, USA</p>
             </div>
             <div id="addCity">
                 <p style={{ marginTop: "40%", marginBottom: "10%", fontSize: "30px" }}>+</p>
