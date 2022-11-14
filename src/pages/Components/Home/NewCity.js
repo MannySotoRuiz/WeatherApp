853635dcc93c966952cf2eb1940ec7891cf03dec
MannySotoRuiz <72245427+MannySotoRuiz@users.noreply.github.com>
2022-11-14 18:09:56 -0500

    started working with unsplash api for the city images

--- src/pages/Components/Home/NewCity.js
+++ src/pages/Components/Home/NewCity.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import "../../../newcitypopup.css";
 import { useState } from "react";
+// import { ImageContext } from "./HomeLeft";
+// import Image from "./Image";
 
 const NewCity = () => {
 
+    // const { response, isLoading } = useContext(ImageContext);
+    
     const closeAddCityPopup = e => {
         let userClicked = e.currentTarget;
         userClicked.parentNode.parentNode.parentNode.classList.add("hidden");
@@ -18,10 +22,24 @@ const NewCity = () => {
     };
 
     const handleAddCity = (e) => {
-        console.log(search, search.length);
         if (search.length === 0) {
             document.getElementById("errorInput").classList.remove("hidden");
-        } 
+        } else {
+            let clientID = "KD3JlHXUemNJy8AIoBejnopOYu4gbmvTsuoal9N4jZk";
+            // let endPoint = `https://api.unsplash.com/photos/random/?client_id=${clientID}`;
+            let endPoint = `https://api.unsplash.com/search/photos?page1&query=newyork&client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+
+            let getAll = document.querySelectorAll(".cityPic");
+
+            fetch(endPoint)
+                .then(function (response) {
+                     return response.json();
+                })
+                .then(function (jsonData) {
+                    console.log(jsonData.results[1].urls.regular);
+                    console.log(getAll[0].children);
+                })
+        }
     };
 
     return (
@@ -32,9 +50,10 @@ const NewCity = () => {
                 </div>
                 <div className="innerInfo">
                     <h3>Add a new city to save</h3>
-                    <input type="text" id="addCitySearch" name="addCitySearch" placeholder="Ex: Barcelona, Spain" value={search} onChange={handleChange}/>
+                    <input type="search" id="addCitySearch" name="addCitySearch" placeholder="Ex: Barcelona, Spain" value={search} onChange={handleChange}/>
                     <button onClick={handleAddCity} className="disabled" id="submitNewCity">Submit</button>
                     <h4 className="hidden" id="errorInput">Please try again</h4>
+                    {/* {response.map((data, key) => <Image key={key} data={data}/>)} */}
                 </div>
             </div>
             <div className="backgroundPopup"></div>
