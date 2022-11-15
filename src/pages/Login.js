d0c4030ca36e30e243e148e6a164c69bb126e781
Amaanmkhwaja <amaanmkhwaja@gmail.com>
2022-11-15 01:40:34 -0500

    Created Login and Signup pages

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,95 @@
+import React, { useState } from "react";
+import '../login.css';
 import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
 
+export default function (props) {
+    let [authMode, setAuthMode] = useState("signin")
     let navigate = useNavigate();
-
-    return (
-        <div id="login">
-            <h1>THIS IS LOGIN PAGE</h1>
-            <button onClick={() => {
-                navigate("/accountsettings");
-            }}>Login</button>
+  
+    const changeAuthMode = () => {
+      setAuthMode(authMode === "signin" ? "signup" : "signin")
+    }
+  
+    if (authMode === "signin") {
+      return (
+        <div className="Auth-form-container">
+          <form className="Auth-form">
+            <div className="Auth-form-content">
+              <h3 className="Auth-form-title">Log In</h3>
+              <div className="text-center">
+                Not registered yet?{" "}
+                <span className="link-primary" onClick={changeAuthMode}>
+                <a href="#">Sign Up</a>
+                </span>
+              </div>
+              <div className="form-group mt-3">
+                <label>Username</label>
+                <input
+                  type="text"
+                  className="form-control mt-1"
+                  placeholder="Enter Username"
+                />
+              </div>
+              <div className="form-group mt-3">
+                <label>Password</label>
+                <input
+                  type="password"
+                  className="form-control mt-1"
+                  placeholder="Enter password"
+                />
+              </div>
+              <div className="d-grid gap-2 mt-3">
+                <button onClick={() => {
+                        navigate("/accountsettings");
+                    }}>Log In</button>
+              </div>
+              <p className="text-center mt-2">
+                Forgot <a href="#">password?</a>
+              </p>
+            </div>
+          </form>
         </div>
-    );
-};
-
-export default Login;
+      )
+    }
+  
+    return (
+      <div className="Auth-form-container">
+        <form className="Auth-form">
+          <div className="Auth-form-content">
+            <h3 className="Auth-form-title">Sign Up</h3>
+            <div className="text-center">
+              Already registered?{" "}
+              <span className="link-primary" onClick={changeAuthMode}>
+              <a href="#">Log In</a>
+              </span>
+            </div>
+            <div className="form-group mt-3">
+              <label>Username</label>
+              <input
+                type="text"
+                className="form-control mt-1"
+                placeholder="Username"
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Password"
+              />
+            </div>
+            <div className="d-grid gap-2 mt-3">
+                <button onClick={() => {
+                    navigate("/login");
+                }}>Sign up</button>
+            </div>
+            <p className="text-center mt-2">
+              Forgot <a href="#">password?</a>
+            </p>
+          </div>
+        </form>
+      </div>
+    )
+  }
