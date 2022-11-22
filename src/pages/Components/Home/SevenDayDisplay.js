9b4b75ab2339daa8e2f7e798fda2665085423864
MannySotoRuiz <72245427+MannySotoRuiz@users.noreply.github.com>
2022-11-22 01:19:07 -0500

    finished displaying 7 day forecast with api call

--- src/pages/Components/Home/SevenDayDisplay.js
+++ src/pages/Components/Home/SevenDayDisplay.js
@@ -37,17 +37,17 @@ const SevenDayDisplay = () => {
                             <div className="dayText">{currentDay[0]}</div>
                             <div className="rainChance">
                                 <div className="rainDropImg"><img src={rainDropImg} alt="rain %"/></div>
-                                <div>69%</div>
+                                <div>{currentDay[1]}%</div>
                             </div>
                             <div className="generalWeather"><img src={currentDay[2]} alt="weather description img"/></div>
                             <div className="lowTempContainer">Lowest:
-                                <div className="lowestTemp">69</div>
+                                <div className="lowestTemp">{currentDay[3]}</div>
                                 <span>&#176;</span>
                                 <div className="fahrenheitDisplay">F</div>
                                 <div className="celsiusDisplay hidden">C</div>
                             </div>
                             <div className="highTempContainer">Highest:
-                                <div className="highestTemp">{currentDay[1]}</div>
+                                <div className="highestTemp">{currentDay[4]}</div>
                                 <span>&#176;</span>
                                 <div className="fahrenheitDisplay">F</div>
                                 <div className="celsiusDisplay hidden">C</div>
@@ -58,158 +58,6 @@ const SevenDayDisplay = () => {
                 })}
             </div>
         );
-
-    // return (
-    //     <div className="sevenDayDisplay">
-    //         <div className="eachDayin7Days">
-    //             <div className="dayText">Sunday</div>
-    //             <div className="rainChance">
-    //                 <div className="rainDropImg"><img src={rainDropImg} alt="rainDropImg"/></div>
-    //                 <div>54%</div>
-    //             </div>
-    //             <div className="generalWeather"><img src={sunImg} alt="sunImg"/></div>
-    //             <div className="lowTempContainer">Lowest:
-    //                 <div className="lowestTemp">55</div>
-    //                 <span>&#176;</span>
-    //                 <div className="fahrenheitDisplay">F</div>
-    //                 <div className="celsiusDisplay hidden">C</div>
-    //             </div>
-    //             <div className="highTempContainer">Highest:
-    //                 <div className="highestTemp">75</div>
-    //                 <span>&#176;</span>
-    //                 <div className="fahrenheitDisplay">F</div>
-    //                 <div className="celsiusDisplay hidden">C</div>
-    //             </div>
-    //             <button onClick={addPopup} className="button-9">Recommended Clothing</button>
-    //         </div>
-    //         <div className="eachDayin7Days">
-    //             <div className="dayText">Monday</div>
-    //             <div className="rainChance">
-    //                 <div className="rainDropImg"><img src={rainDropImg} alt="rainDropImg"/></div>
-    //                 <div>54%</div>
-    //             </div>
-    //             <div className="generalWeather"><img src={cloudyNoSunImg} alt="cloudyNoSunImg"/></div>
-    //             <div className="lowTempContainer">Lowest:
-    //                 <div className="lowestTemp">55</div>
-    //                 <span>&#176;</span>
-    //                 <div className="fahrenheitDisplay">F</div>
-    //                 <div className="celsiusDisplay hidden">C</div>
-    //             </div>
-    //             <div className="highTempContainer">Highest:
-    //                 <div className="highestTemp">75</div>
-    //                 <span>&#176;</span>
-    //                 <div className="fahrenheitDisplay">F</div>
-    //                 <div className="celsiusDisplay hidden">C</div>
-    //             </div>
-    //             <button onClick={addPopup} className="button-9">Recommended Clothing</button>
-    //         </div>
-    //         <div className="eachDayin7Days">
-    //             <div className="dayText">Tuesday</div>
-    //             <div className="rainChance">
-    //                 <div className="rainDropImg"><img src={rainDropImg} alt="rainDropImg"/></div>
-    //                 <div>54%</div>
-    //             </div>
-    //             <div className="generalWeather"><img src={cloudyWithSunImg} alt="cloudyWithSunImg"/></div>
-    //             <div className="lowTempContainer">Lowest:
-    //                 <div className="lowestTemp">55</div>
-    //                 <span>&#176;</span>
-    //                 <div className="fahrenheitDisplay">F</div>
-    //                 <div className="celsiusDisplay hidden">C</div>
-    //             </div>
-    //             <div className="highTempContainer">Highest:
-    //                 <div className="highestTemp">75</div>
-    //                 <span>&#176;</span>
-    //                 <div className="fahrenheitDisplay">F</div>
-    //                 <div className="celsiusDisplay hidden">C</div>
-    //             </div>
-    //             <button onClick={addPopup} className="button-9">Recommended Clothing</button>
-    //         </div>
-    //         <div className="eachDayin7Days">
-    //             <div className="dayText">Wednesday</div>
-    //             <div className="rainChance">
-    //                 <div className="rainDropImg"><img src={rainDropImg} alt="rainDropImg"/></div>
-    //                 <div>54%</div>
-    //             </div>
-    //             <div className="generalWeather"><img src={cloudyRainImg} alt="cloudyWithSunImg"/></div>
-    //             <div className="lowTempContainer">Lowest:
-    //                 <div className="lowestTemp">55</div>
-    //                 <span>&#176;</span>
-    //                 <div className="fahrenheitDisplay">F</div>
-    //                 <div className="celsiusDisplay hidden">C</div>
-    //             </div>
-    //             <div className="highTempContainer">Highest:
-    //                 <div className="highestTemp">75</div>
-    //                 <span>&#176;</span>
-    //                 <div className="fahrenheitDisplay">F</div>
-    //                 <div className="celsiusDisplay hidden">C</div>
-    //             </div>
-    //             <button onClick={addPopup} className="button-9">Recommended Clothing</button>
-    //         </div>
-    //         <div className="eachDayin7Days">
-    //             <div className="dayText">Thursday</div>
-    //             <div className="rainChance">
-    //                 <div className="rainDropImg"><img src={rainDropImg} alt="rainDropImg"/></div>
-    //                 <div>54%</div>
-    //             </div>
-    //             <div className="generalWeather"><img src={sunImg} alt="sunImg"/></div>
-    //             <div className="lowTempContainer">Lowest:
-    //                 <div className="lowestTemp">55</div>
-    //                 <span>&#176;</span>
-    //                 <div className="fahrenheitDisplay">F</div>
-    //                 <div className="celsiusDisplay hidden">C</div>
-    //             </div>
-    //             <div className="highTempContainer">Highest:
-    //                 <div className="highestTemp">75</div>
-    //                 <span>&#176;</span>
-    //                 <div className="fahrenheitDisplay">F</div>
-    //                 <div className="celsiusDisplay hidden">C</div>
-    //             </div>
-    //             <button onClick={addPopup} className="button-9">Recommended Clothing</button>
-    //         </div>
-    //         <div className="eachDayin7Days">
-    //             <div className="dayText">Friday</div>
-    //             <div className="rainChance">
-    //                 <div className="rainDropImg"><img src={rainDropImg} alt="rainDropImg"/></div>
-    //                 <div>54%</div>
-    //             </div>
-    //             <div className="generalWeather"><img src={sunImg} alt="sunImg"/></div>
-    //             <div className="lowTempContainer">Lowest:
-    //                 <div className="lowestTemp">55</div>
-    //                 <span>&#176;</span>
-    //                 <div className="fahrenheitDisplay">F</div>
-    //                 <div className="celsiusDisplay hidden">C</div>
-    //             </div>
-    //             <div className="highTempContainer">Highest:
-    //                 <div className="highestTemp">75</div>
-    //                 <span>&#176;</span>
-    //                 <div className="fahrenheitDisplay">F</div>
-    //                 <div className="celsiusDisplay hidden">C</div>
-    //             </div>
-    //             <button onClick={addPopup} className="button-9">Recommended Clothing</button>
-    //         </div>
-    //         <div className="eachDayin7Days">
-    //             <div className="dayText">Saturday</div>
-    //             <div className="rainChance">
-    //                 <div className="rainDropImg"><img src={rainDropImg} alt="rainDropImg"/></div>
-    //                 <div>54%</div>
-    //             </div>
-    //             <div className="generalWeather"><img src={sunImg} alt="sunImg"/></div>
-    //             <div className="lowTempContainer">Lowest:
-    //                 <div className="lowestTemp">55</div>
-    //                 <span>&#176;</span>
-    //                 <div className="fahrenheitDisplay">F</div>
-    //                 <div className="celsiusDisplay hidden">C</div>
-    //             </div>
-    //             <div className="highTempContainer">Highest:
-    //                 <div className="highestTemp">75</div>
-    //                 <span>&#176;</span>
-    //                 <div className="fahrenheitDisplay">F</div>
-    //                 <div className="celsiusDisplay hidden">C</div>
-    //             </div>
-    //             <button onClick={addPopup} className="button-9">Recommended Clothing</button>
-    //         </div>
-    //     </div>
-    // );
 };
 
 export default SevenDayDisplay;
